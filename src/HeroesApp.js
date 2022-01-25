cdf78cf1b308e82068897ba49107e29206badb3d
mrround <antonioredondoperez@gmail.com>
2022-01-25 22:58:16 +0100

    Grid wordle

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -2,9 +2,54 @@ import './HeroesApp.css';
 
 function HeroesApp() {
   return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
+    <div className='w-100 position-absolute top-50 start-50 translate-middle'>
+     <div className="container text-center ">
+        <div className="row m-0 linea d-flex justify-content-center">
+          <div className="m-1 celda celda-incorrecta">P</div>
+          <div className="m-1 celda celda-incorrecta">O</div>
+          <div className="m-1 celda celda-incorrecta">I</div>
+          <div className="m-1 celda celda-incorrecta">N</div>
+          <div className="m-1 celda celda-incorrecta">T</div>
+        </div>
+        <div className="row m-0 linea d-flex justify-content-center">
+          <div className="m-1 celda celda-correcta">S</div>
+          <div className="m-1 celda celda-correcta">U</div>
+          <div className="m-1 celda celda-correcta">G</div>
+          <div className="m-1 celda celda-correcta">A</div>
+          <div className="m-1 celda celda-correcta">R</div>
+        </div>
+        <div className="row m-0 linea d-flex justify-content-center">
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+        </div>
+        <div className="row m-0 linea d-flex justify-content-center">
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+        </div>
+        <div className="row m-0 linea d-flex justify-content-center">
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+        </div>
+        <div className="row m-0 linea d-flex justify-content-center">
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+          <div className="m-1 celda"></div>
+        </div>
+      </div>
+    </div>
+
+    
   );
 }
 
