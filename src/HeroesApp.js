37edfce20f6816422b45915c232de4272da670b4
mrround <antonioredondoperez@gmail.com>
2022-01-25 23:14:39 +0100

    Ajuste para que se vea bien en móvil

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -5,46 +5,46 @@ function HeroesApp() {
     <div className='w-100 position-absolute top-50 start-50 translate-middle'>
      <div className="container text-center ">
         <div className="row m-0 linea d-flex justify-content-center">
-          <div className="m-1 celda celda-incorrecta">P</div>
-          <div className="m-1 celda celda-incorrecta">O</div>
-          <div className="m-1 celda celda-incorrecta">I</div>
-          <div className="m-1 celda celda-incorrecta">N</div>
-          <div className="m-1 celda celda-incorrecta">T</div>
+          <div className="celda celda-incorrecta">P</div>
+          <div className="celda celda-incorrecta">O</div>
+          <div className="celda celda-incorrecta">I</div>
+          <div className="celda celda-incorrecta">N</div>
+          <div className="celda celda-incorrecta">T</div>
         </div>
         <div className="row m-0 linea d-flex justify-content-center">
-          <div className="m-1 celda celda-correcta">S</div>
-          <div className="m-1 celda celda-correcta">U</div>
-          <div className="m-1 celda celda-correcta">G</div>
-          <div className="m-1 celda celda-correcta">A</div>
-          <div className="m-1 celda celda-correcta">R</div>
+          <div className="celda celda-correcta">S</div>
+          <div className="celda celda-correcta">U</div>
+          <div className="celda celda-correcta">G</div>
+          <div className="celda celda-correcta">A</div>
+          <div className="celda celda-correcta">R</div>
         </div>
         <div className="row m-0 linea d-flex justify-content-center">
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
         </div>
         <div className="row m-0 linea d-flex justify-content-center">
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
         </div>
         <div className="row m-0 linea d-flex justify-content-center">
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
         </div>
         <div className="row m-0 linea d-flex justify-content-center">
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
-          <div className="m-1 celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
+          <div className="celda"></div>
         </div>
       </div>
     </div>
