7e096c2b9ae1fcd3fdb3889427c674e54867ed6b
mrround <antonioredondoperez@gmail.com>
2022-01-29 22:54:53 +0100

    Layout

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,7 +4,7 @@ import { Cell } from './Cell';
 export const Row = ({word, index}) => {
     // console.log(word);
   return (
-    <div className="row m-0 linea d-flex justify-content-center" key={index}>
+    <div className="d-flex justify-content-center" key={index}>
         {
             word.map((letter, index) => (
                 <Cell 
