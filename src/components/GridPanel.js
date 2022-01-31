28bf0f379281cbc5d7160dc62edaeb629c6ec238
mrround <antonioredondoperez@gmail.com>
2022-01-31 21:43:16 +0100

    Palabras de golpe y con retardo

--- src/components/GridPanel.js
+++ src/components/GridPanel.js
@@ -3,42 +3,107 @@ import '../HeroesApp.css';
 import { Row } from './Row';
 
 export const GridPanel = () => {
-  const [grid, setGrid] = useState([
+
+  const [grid, setGrid] = useState([ 
                                       [{},{},{},{},{}],                               
                                       [{},{},{},{},{}],                               
                                       [{},{},{},{},{}],
                                       [{},{},{},{},{}],
                                       [{},{},{},{},{}],
                                       [{},{},{},{},{}]
-                                  ]);
+                                    ]);
+  var currentGrid = [ 
+                      [{},{},{},{},{}],                               
+                      [{},{},{},{},{}],                               
+                      [{},{},{},{},{}],
+                      [{},{},{},{},{}],
+                      [{},{},{},{},{}],
+                      [{},{},{},{},{}]
+                    ];
+
+  const [row, setRow] = useState([{}, {}, {}, {}, {}]);
+  const [activeRow, setActiveRow] = useState(0);
+
+  var currentRow = [{}, {}, {}, {}, {}];
 
   const firstWord = [
-      {
-        letter:"P",
-        state:"correct"
-      }, {
-        letter:"O",
-        state:"incorrect"
-      }, {
-        letter:"I",
-        state:"misplaced"
-      }, {
-        letter:"N",
-        state:"incorrect"
-      }, {
-        letter:"T",
-        state:"incorrect"
-      } 
-    ]
-
-  const handleFillWord = (word) => {
-    setGrid(grid => [word, ...grid]);
-    console.log(grid);
+    {
+      letter:"P",
+      state:"correct"
+    }, {
+      letter:"O",
+      state:"incorrect"
+    }, {
+      letter:"I",
+      state:"misplaced"
+    }, {
+      letter:"N",
+      state:"incorrect"
+    }, {
+      letter:"T",
+      state:"incorrect"
+    } 
+  ]
+  
+  const handleFillLetter = (letter) => {
+    var increaseActiveRow = 0;
+    // console.log("currentGrid:");
+    // console.log(currentGrid);
+    setRow(grid[activeRow]);
+    console.log(row);
+    
+    for (var i=0; i < row.length; i++) {
+      if (Object.keys(row[i]).length == 0) {
+        row[i] = letter;
+        grid[activeRow] = row;
+        // console.log("grid");
+        // console.log(grid[activeRow]);
+        // console.log("row");
+        // console.log(row);
+        setGrid(grid);
+        setRow(row);
+        break;
+      } else {
+        // console.log("i: " + i);
+        // console.log("length: " + (currentRow.length - 1));
+        // console.log("activeRow: " + activeRow);
+        if (i === (row.length - 1)) {
+          increaseActiveRow = 1
+        }
+      }
+      // this.setState();
+    }
+  
+    
+    // // console.log("activeRow: " + activeRow);
+    // for (var i=activeRow; i < grid.length; i++) {
+    //   if (Object.keys(grid[i][0]).length == 0) {
+    //     // console.log(currentRow);
+    //     //currentGrid[i] = currentRow;
+    //     break;
+    //   }
+    // }
+    // grid[activeRow] = currentRow;
+    // setGrid(grid);
+    
+    if (increaseActiveRow === 1) {
+      setActiveRow(activeRow + 1) ;
+    }
+    // console.log(grid);
+    
   } 
 
-  useEffect(() => {
-    handleFillWord(firstWord);
-  }, []);
+  // useEffect(() => {
+  //   // handleFillWord(firstWord);
+  //   // hendleLetra({
+  //   //     letter:"F",
+  //   //     state:"correct"
+  //   //   });
+  //   handleFillLetter({
+  //     letter:"F",
+  //     state:"correct"
+  //   });
+  // }, []);
 
   return (
     <div className='w-100 grid-panel'>
@@ -48,8 +113,12 @@ export const GridPanel = () => {
         <Row word={grid[2]} rowIndex={2}/>        
         <Row word={grid[3]} rowIndex={3}/>        
         <Row word={grid[4]} rowIndex={4}/>        
-        <Row word={grid[5]} rowIndex={5}/>        
+        <Row word={grid[5]} rowIndex={5}/>            
       </div>
+      <button onClick={() => handleFillLetter({
+       letter:"F",
+       state:""
+     })}>Añadir</button>
     </div>
 
     
--- src/components/Row.js
+++ src/components/Row.js
@@ -5,7 +5,7 @@ export const Row = ({word, index}) => {
     // console.log(word);
   return (
     <div className="d-flex justify-content-center" key={index}>
-        {
+        {/* {
             word.map((letter, index) => (
                 <Cell 
                     key={index}
@@ -13,7 +13,12 @@ export const Row = ({word, index}) => {
                     letter={letter.letter}
                 />
             ))    
-        }
+        } */}
+        <Cell letter={word[0].letter} state={word[0].state} key={0} />
+        <Cell letter={word[1].letter} state={word[1].state} key={1} />
+        <Cell letter={word[2].letter} state={word[2].state} key={2} />
+        <Cell letter={word[3].letter} state={word[3].state} key={3} />
+        <Cell letter={word[4].letter} state={word[4].state} key={4} />
     </div>
   )
 };
