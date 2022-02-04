22c5111cb803ab51d1d93a95d5a66d087f07599f
mrround <antonioredondoperez@gmail.com>
2022-02-04 21:16:28 +0100

    primera versión operativa

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -17,48 +17,59 @@ export const HeroesApp = () => {
 
   const [row, setRow] = useState([{}, {}, {}, {}, {}]);
   const [activeRow, setActiveRow] = useState(0);
+  const [gameState, setGameState] = useState("playing");
   const secretWord = "POINT";
 
   const handleKeyInput = (letter) => {
-    
-    if (letter === "ENTER") {
-      checkWord();
-    } else if (letter === "DELETE") {
-      for (var i=row.length - 1; i >= 0 ; i--) {
-        if (Object.keys(row[i]).length > 0) {
-          row[i] = {};
-          break;
-        }
-      }
-      setGrid(grid => [...grid, row]);
-    } else {
-
-      for (var i=0; i < row.length; i++) {
-
-        if (Object.keys(row[i]).length == 0) {
-          row[i] = {
-            letter,
-            state: ""
-          };
-          break;
-        } else {
-          if (i === (row.length - 1)) { 
-            setActiveRow(activeRow + 1) ;
+    if (gameState === "playing") {
+      if (letter === "ENTER") {
+        checkWord();
+      } else if (letter === "DELETE") {
+        for (var i=row.length - 1; i >= 0 ; i--) {
+          if (Object.keys(row[i]).length > 0) {
+            row[i] = {};
+            break;
           }
         }
-      }
-    setGrid(grid => [...grid, row]);
+        setGrid(grid => [...grid, row]);
+      } else {
+
+        for (var i=0; i < row.length; i++) {
+
+          if (Object.keys(row[i]).length == 0) {
+            row[i] = {
+              letter,
+              state: ""
+            };
+            break;
+          } else {
+            if (i === (row.length - 1)) { 
+              // setActiveRow(activeRow + 1) ;
+            }
+          }
+        }
+      setGrid(grid => [...grid, row]);
     }
+   }
   } 
 
   const checkWord = () => {
-    console.log("checkWord");
- 
+    var corrects = 0;
+    // console.log("checkWord");
     if (Object.keys(row[row.length - 1]).length > 0) {
       console.log("comprobamos palabra!");
       for (var i=0; i < row.length; i++) {
-        console.log("comprobamos la: " + i);
-        // checkLetter(letter, i, secretWord)
+
+        // console.log("comprobamos la: " + row[i].letter);
+        const state = checkLetter(row[i].letter, i, secretWord);
+        if (state === "correct") {
+          corrects ++;
+        }
+        row[i].state = state;
+      }
+      setActiveRow(activeRow + 1);
+      if (corrects == 5) {
+        setGameState("over");
       }
     }
 
